c9055282da370259833421dbddcade1917f3398f
Matt Westcott <matt@west.co.tt>
2019-11-12 17:52:04 +0000

    Add test for API

--- tests/urls.py
+++ tests/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls import include, url
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
+from wagtail_transfer import urls as wagtailtransfer_urls
 
 
 urlpatterns = [
     url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^wagtail-transfer/', include(wagtailtransfer_urls)),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's serving mechanism
