4368ebfc5a9539c72b4a21ae238cf6b5d67f80e5
Matt Westcott <matt@west.co.tt>
2019-11-12 17:52:04 +0000

    Add test for API

---
+++ tests/tests/test_api.py
@@ -0,0 +1,30 @@
+import json
+
+from django.test import TestCase
+
+
+class TestPagesApi(TestCase):
+    fixtures = ['test.json']
+
+    def test_pages_api(self):
+        response = self.client.get('/wagtail-transfer/api/pages/2/')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+
+        ids_for_import = data['ids_for_import']
+        self.assertIn(['wagtailcore.page', 2], ids_for_import)
+        self.assertNotIn(['wagtailcore.page', 1], ids_for_import)
+
+        homepage = None
+        for obj in data['objects']:
+            if obj['model'] == 'tests.simplepage' and obj['pk'] == 2:
+                homepage = obj
+                break
+
+        self.assertTrue(homepage)
+        self.assertEqual(homepage['parent_id'], 1)
+        self.assertEqual(homepage['fields']['intro'], "This is the homepage")
+
+        mappings = data['mappings']
+        self.assertIn(['wagtailcore.page', 2, "22222222-2222-2222-2222-222222222222"], mappings)
+        self.assertIn(['tests.advert', 1, "adadadad-1111-1111-1111-111111111111"], mappings)
