7baab74d1071ec07eeb037804135bb13fcee943d
Karl Hobley <karl@kaed.uk>
2019-11-14 18:17:00 +0000

    Add chooser API

--- wagtail_transfer/urls.py
+++ wagtail_transfer/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
 
+from .vendor.wagtail_api_v2.router import WagtailAPIRouter
+
 from wagtail_transfer import views
+
+chooser_api = WagtailAPIRouter('wagtail_transfer_page_chooser_api')
+chooser_api.register_endpoint('pages', views.PageChooserAPIViewSet)
 
 urlpatterns = [
     url(r'^api/pages/(\d+)/$', views.pages_for_export, name='wagtail_transfer_pages'),
+    url(r'^api/chooser/', chooser_api.urls),
 ]
--- wagtail_transfer/vendor/wagtail_admin_api/serializers.py
+++ wagtail_transfer/vendor/wagtail_admin_api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework.fields import Field, ReadOnlyField
 
 from wagtail.core.models import Page
 
-from ..wagtail_api_v2.serializers import PageSerializer
+from ..wagtail_api_v2.serializers import PageSerializer, get_serializer_class
 from ..wagtail_api_v2.utils import get_full_url
 
 
@@ -77,8 +77,47 @@ class PageDescendantsField(Field):
         ])
 
 
+class PageAncestorsField(Field):
+    """
+    Serializes the page's ancestry.
+    Example:
+    "ancestry": [
+        {
+            "id": 1,
+            "meta": {
+                "type": "wagtailcore.Page",
+                "detail_url": "/api/v1/pages/1/"
+            },
+            "title": "Root"
+        },
+        {
+            "id": 2,
+            "meta": {
+                "type": "home.HomePage",
+                "detail_url": "/api/v1/pages/2/"
+            },
+            "title": "Home"
+        }
+    ]
+    """
+    def get_attribute(self, instance):
+        return instance
+
+    def serialize_page(self, page):
+        serializer_class = get_serializer_class(page.__class__, ['id', 'type', 'detail_url', 'html_url', 'title'], meta_fields=['type', 'detail_url', 'html_url'], base=PageSerializer)
+        serializer = serializer_class(context=self.context)
+        return serializer.to_representation(page)
+
+    def to_representation(self, page):
+        return [
+            self.serialize_page(ancestor)
+            for ancestor in page.get_ancestors()
+        ]
+
+
 class AdminPageSerializer(PageSerializer):
     status = PageStatusField(read_only=True)
     children = PageChildrenField(read_only=True)
     descendants = PageDescendantsField(read_only=True)
+    ancestors = PageAncestorsField(read_only=True)
     admin_display_title = ReadOnlyField(source='get_admin_display_title')
