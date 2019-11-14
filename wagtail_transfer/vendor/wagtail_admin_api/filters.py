7c97c5d197b2ced1ef8fda2accd77a7881189b42
Karl Hobley <karl@kaed.uk>
2019-11-14 18:16:40 +0000

    Vendor Wagtail API modules

---
+++ wagtail_transfer/vendor/wagtail_admin_api/filters.py
@@ -0,0 +1,37 @@
+from rest_framework.filters import BaseFilterBackend
+
+from wagtail.core import hooks
+
+from ..wagtail_api_v2.utils import BadRequestError, parse_boolean
+
+
+class HasChildrenFilter(BaseFilterBackend):
+    """
+    Filters the queryset by checking if the pages have children or not.
+    This is useful when you want to get just the branches or just the leaves.
+    """
+    def filter_queryset(self, request, queryset, view):
+        if 'has_children' in request.GET:
+            try:
+                has_children_filter = parse_boolean(request.GET['has_children'])
+            except ValueError:
+                raise BadRequestError("has_children must be 'true' or 'false'")
+
+            if has_children_filter is True:
+                return queryset.filter(numchild__gt=0)
+            else:
+                return queryset.filter(numchild=0)
+
+        return queryset
+
+
+class ForExplorerFilter(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        if not hasattr(queryset, '_filtered_by_child_of'):
+            raise BadRequestError("filtering without child_of is not supported")
+
+        parent_page = queryset._filtered_by_child_of
+        for hook in hooks.get_hooks('construct_explorer_page_queryset'):
+            queryset = hook(parent_page, queryset, request)
+
+        return queryset
---
+++ wagtail_transfer/vendor/wagtail_admin_api/views.py
@@ -0,0 +1,119 @@
+from collections import OrderedDict
+
+from rest_framework.authentication import SessionAuthentication
+
+from wagtail.admin.navigation import get_explorable_root_page
+from wagtail.core.models import Page, UserPagePermissionsProxy
+
+from ..wagtail_api_v2.utils import filter_page_type
+from ..wagtail_api_v2.views import PagesAPIViewSet
+from .filters import ForExplorerFilter, HasChildrenFilter
+from .serializers import AdminPageSerializer
+
+
+class PagesAdminAPIViewSet(PagesAPIViewSet):
+    base_serializer_class = AdminPageSerializer
+    authentication_classes = [SessionAuthentication]
+
+    # Add has_children filter
+    filter_backends = PagesAPIViewSet.filter_backends + [
+        HasChildrenFilter,
+    ]
+
+    meta_fields = PagesAPIViewSet.meta_fields + [
+        'latest_revision_created_at',
+        'status',
+        'children',
+        'descendants',
+        'parent',
+    ]
+
+    body_fields = PagesAPIViewSet.body_fields + [
+        'admin_display_title',
+    ]
+
+    listing_default_fields = PagesAPIViewSet.listing_default_fields + [
+        'latest_revision_created_at',
+        'status',
+        'children',
+        'admin_display_title',
+    ]
+
+    # Allow the parent field to appear on listings
+    detail_only_fields = []
+
+    known_query_parameters = PagesAPIViewSet.known_query_parameters.union([
+        'has_children'
+    ])
+
+    def get_root_page(self):
+        """
+        Returns the page that is used when the `&child_of=root` filter is used.
+        """
+        return Page.get_first_root_node()
+
+    def get_base_queryset(self, models=None):
+        """
+        Returns a queryset containing all pages that can be seen by this user.
+
+        This is used as the base for get_queryset and is also used to find the
+        parent pages when using the child_of and descendant_of filters as well.
+        """
+        if models is None:
+            models = [Page]
+
+        if len(models) == 1:
+            queryset = models[0].objects.all()
+        else:
+            queryset = Page.objects.all()
+
+            # Filter pages by specified models
+            queryset = filter_page_type(queryset, models)
+
+        return queryset
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # Hide root page
+        # TODO: Add "include_root" flag
+        queryset = queryset.exclude(depth=1).specific()
+
+        return queryset
+
+    def get_type_info(self):
+        types = OrderedDict()
+
+        for name, model in self.seen_types.items():
+            types[name] = OrderedDict([
+                ('verbose_name', model._meta.verbose_name),
+                ('verbose_name_plural', model._meta.verbose_name_plural),
+            ])
+
+        return types
+
+    def listing_view(self, request):
+        response = super().listing_view(request)
+        response.data['__types'] = self.get_type_info()
+        return response
+
+    def detail_view(self, request, pk):
+        response = super().detail_view(request, pk)
+        response.data['__types'] = self.get_type_info()
+        return response
+
+
+class PagesForExplorerAdminAPIViewSet(PagesAdminAPIViewSet):
+    # Add for_explorer filter
+    filter_backends = PagesAPIViewSet.filter_backends + [
+        ForExplorerFilter,
+    ]
+
+    def get_root_page(self):
+        return get_explorable_root_page(self.request.user)
+
+    def get_base_queryset(self, models=None):
+        queryset = super().get_base_queryset(models=models)
+        user_perms = UserPagePermissionsProxy(self.request.user)
+        queryset = queryset & user_perms.explorable_pages()
+        return queryset
