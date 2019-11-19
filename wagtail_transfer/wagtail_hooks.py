ac8fcb7124965ea9fdcc8211e9a3be5225d32558
Karl Hobley <karl@kaed.uk>
2019-11-19 13:59:24 +0000

    Add menu item

--- wagtail_transfer/wagtail_hooks.py
+++ wagtail_transfer/wagtail_hooks.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.urls import reverse
 
+from wagtail.admin.menu import MenuItem
 from wagtail.core import hooks
 
 from . import admin_urls
@@ -10,3 +13,13 @@ def register_admin_urls():
     return [
         url(r'^wagtail-transfer/', include(admin_urls, namespace='wagtail_transfer_admin')),
     ]
+
+
+class WagtailTransferMenuItem(MenuItem):
+    def is_shown(self, request):
+        return bool(getattr(settings, 'WAGTAILTRANSFER_SOURCES', None))
+
+
+@hooks.register('register_admin_menu_item')
+def register_admin_menu_item():
+    return WagtailTransferMenuItem('Import pages', reverse('wagtail_transfer_admin:choose_page'), order=10000)
