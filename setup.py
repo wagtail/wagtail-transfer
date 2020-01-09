25047e936e15c978697a0c6c129d9cd78a78bc1b
Matt Westcott <matt@west.co.tt>
2020-01-09 14:44:38 +0000

    Set up MANIFEST.in and setup.py for packaging

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Matthew Westcott',
     author_email='matthew.westcott@torchbox.com',
     url='https://github.com/wagtail/wagtail-transfer',
-    packages=find_packages(),
+    packages=find_packages(exclude=('tests',)),
     include_package_data=True,
     install_requires=[
     ],
