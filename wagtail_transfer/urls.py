8d9406bdfd733ac549092d4e66b82aef35e64764
Kalob Taulien <kalob.taulien@gmail.com>
2020-04-06 13:08:56 -0600

    Api endpoints for future model chooser

--- wagtail_transfer/urls.py
+++ wagtail_transfer/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from .vendor.wagtail_api_v2.router import WagtailAPIRouter
 
 from wagtail_transfer import views
+from wagtail_transfer.vendor.wagtail_api_v2.views import ModelsAPIViewSet
 
 chooser_api = WagtailAPIRouter('wagtail_transfer_page_chooser_api')
 chooser_api.register_endpoint('pages', views.PageChooserAPIViewSet)
+chooser_api.register_endpoint('models', ModelsAPIViewSet)
 
 urlpatterns = [
     url(r'^api/pages/(\d+)/$', views.pages_for_export, name='wagtail_transfer_pages'),
