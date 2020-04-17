b2010d919a82b23d8e508b6f36cf4581809e6c4b
Kalob Taulien <kalob.taulien@gmail.com>
2020-04-17 11:57:25 -0600

    Model Object api pagination

--- wagtail_transfer/vendor/wagtail_api_v2/pagination.py
+++ wagtail_transfer/vendor/wagtail_api_v2/pagination.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 
 from django.conf import settings
-from rest_framework.pagination import BasePagination
+from rest_framework.pagination import BasePagination, PageNumberPagination
 from rest_framework.response import Response
 
 from .utils import BadRequestError
@@ -48,46 +48,21 @@ class WagtailPagination(BasePagination):
 
 
 
-class ModelPagination(BasePagination):
-    # TODO Support model and model object pagination.
-    def paginate_queryset(self, queryset, request, view=None):
-        limit_max = getattr(settings, 'WAGTAILAPI_LIMIT_MAX', 20)
+class ModelPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 200
+    last_page_strings = ('the_end',)
 
-        try:
-            offset = int(request.GET.get('offset', 0))
-            if offset < 0:
-                raise ValueError()
-        except ValueError:
-            raise BadRequestError("offset must be a positive integer")
-
-        try:
-            limit_default = 20 if not limit_max else min(20, limit_max)
-            limit = int(request.GET.get('limit', limit_default))
-            if limit < 0:
-                raise ValueError()
-        except ValueError:
-            raise BadRequestError("limit must be a positive integer")
-
-        if limit_max and limit > limit_max:
-            raise BadRequestError(
-                "limit cannot be higher than %d" % limit_max)
-
-        start = offset
-        stop = offset + limit
-
-        self.view = view
-        try:
-            total = queryset.count()
-        except TypeError:
-            total = len(queryset)
-        self.total_count = total
-        return queryset[start:stop]
 
     def get_paginated_response(self, data):
-        data = OrderedDict([
-            ('meta', OrderedDict([
-                ('total_count', self.total_count),
-            ])),
-            ('items', data),
-        ])
+        print("GETTING PAGINATED RESPONSE METHOD")
+        data = {
+            "meta": {
+                "total_count": self.page.paginator.count,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+            },
+            "items": data
+        }
         return Response(data)
