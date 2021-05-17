e46606bbf5909d68e36147336740266d5c63726f
Nick Smith <nick.smith@torchbox.com>
2021-05-17 17:17:35 +0100

    Ensure content type is available during migration

--- wagtail_transfer/migrations/0003_permissions.py
+++ wagtail_transfer/migrations/0003_permissions.py
@@ -1,7 +1,14 @@
+from django.contrib.contenttypes.management import create_contenttypes
 from django.db import migrations
 
 
 def create_import_permission(apps, schema_editor):
+    app_config = apps.get_app_config("wagtail_transfer")
+    # Ensure content types from previous migrations are created. This is normally done
+    # in a post_migrate signal, see
+    # https://github.com/django/django/blob/3.2/django/contrib/contenttypes/apps.py#L21
+    app_config.models_module = app_config.models_module or True
+    create_contenttypes(app_config)
     ContentType = apps.get_model("contenttypes", "ContentType")
     content_type = ContentType.objects.get(
         app_label="wagtail_transfer", model="idmapping"
@@ -21,7 +28,8 @@ def delete_import_permission(apps, schema_editor):
     )
     Permission = apps.get_model("auth", "Permission")
     permission = Permission.objects.filter(
-        codename="wagtailtransfer_can_import", content_type=content_type,
+        codename="wagtailtransfer_can_import",
+        content_type=content_type,
     )
     permission.delete()
 
