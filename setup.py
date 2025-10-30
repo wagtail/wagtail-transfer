fc146eafaf399781241690214f9f9562e1e64313
Matthew Westcott <matthew@west.co.tt>
2025-10-30 14:14:51 +0000

    Version bump to 0.11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wagtail-transfer',
-    version='0.10',
+    version='0.11',
     description="Content transfer for Wagtail",
     author='Matthew Westcott',
     author_email='matthew.westcott@torchbox.com',
